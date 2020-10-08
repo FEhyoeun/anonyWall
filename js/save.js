8d0b3fd9d98ed35144df25cc88e5da13d087ab2e
WDkimhyoeun <66587298+WDkimhyoeun@users.noreply.github.com>
2020-10-08 13:40:03 +0900

    save 기능 구현중. 데이터를 localStorage에 넣는 것까지는 완료

--- js/save.js
+++ js/save.js
@@ -1,15 +1,32 @@
 const saveBtn = document.querySelector(".save-button");
+
+saveBtn.addEventListener('click', saveContents);
+
+// 입력 받은 데이터를 저장
+function saveDatas {
+    console.log(body);
+}
+
 // 입력 받은 내용
 let inputContents = [];
 
-// 보내는 사람 이름
+function saveContents() {
+    inputContents = document.querySelector(".form-input").value;
+    localStorage.setItem('saveBody', inputContents);
+    const body = localStorage.getItem('saveBody');
+    console.log(body);
+}
+
+
+// 입력 받은 이름
 let InputSender = [];
 
-saveBtn.addEventListener('click', saveData);
+saveBtn.addEventListener('click', savaSender);
 
-function saveData() {
-    inputContents = document.querySelector(".form-input").value;
+function savaSender() {
     InputSender = document.querySelector(".sender-name").value;
-    
+    localStorage.setItem('saveName', InputSender);
+    const name = localStorage.getItem('saveName');
+    console.log(name);
 }
 
