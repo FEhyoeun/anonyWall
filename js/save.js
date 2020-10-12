54f0f78bf43ffbdba996efb2629e23f0faef641b
WDkimhyoeun <66587298+WDkimhyoeun@users.noreply.github.com>
2020-10-12 14:32:14 +0900

    json형태로 변환까지 완료

--- js/save.js
+++ js/save.js
@@ -1,28 +1,58 @@
+'use strict';
+
 const saveBtn = document.querySelector(".save-button");
 
+
 // 폼 만들기
-
-
+function makeForm() {
+   
+}
 
 // 입력받은 데이터를 저장
- function saveDatas() {
+function saveDatas() {
     saveContents();
     savaSender();
- }
+    }
 
-// 입력받은 내용
+// 입력받은 내용을 저장하기
 let inputContents = [];
 
 saveBtn.addEventListener('click', saveContents);
 
 function saveContents() {
     inputContents = document.querySelector(".form-input").value;
-    localStorage.setItem('saveBody', inputContents);
-    const body = localStorage.getItem('saveBody');
-    console.log(body);
+    localStorage.setItem('saveBody', JSON.stringify(inputContents));
+    const body = JSON.parse(localStorage.getItem('saveBody'));
+    
+
+    // const body1 = document.createElement("div");
+    // body1.classList.add("column");
+    
+    // const body2 = document.createElement("div");
+    // body2.classList.add("card");
+    // body1.appendChild(body2);
+
+    // const body3 = document.createElement("div");
+    // body3.classList.add("content");
+    // body2.appendChild(body3);
+
+    // const body4 = document.createElement("h2");
+    // body4.classList.add("content-name");
+    // body3.appendChild(body4);
+
+    // const body5 = document.createElement("p");
+    // body5.classList.add("content-body");
+    // body4.appendChild(body5);
+    // body5.innerText = body;
+
+    // const row = document.querySelector(".row");                                             
+    // console.log(row);
+
+    //row.appendChild(body1);
+
 }
 
-// 입력받은 이름
+// 입력받은 이름을 저장하기
 let InputSender = [];
 
 saveBtn.addEventListener('click', savaSender);
